7406ed51c216d163adeaa2fde06a70917e9f1740
Morsed <mdmorsedalam324@gmail.com>
2024-01-25 00:00:59 +0600

    Use cors in env

--- src/middlewares/applyMiddleWares.js
+++ src/middlewares/applyMiddleWares.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const applyMiddleWares = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  app.use(
+    cors({
+      origin: [process.env.LOCAL_CLIENT, process.env.CLIENT],
+      credentials: true,
+    })
+  );
 };
 module.exports = applyMiddleWares;
