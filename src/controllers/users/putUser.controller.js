598b7264baf376adf7a4dc7a4afe98a341807e9a
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2024-02-08 15:59:08 +0600

    added get and put oparetion in user

--- src/controllers/users/putUser.controller.js
+++ src/controllers/users/putUser.controller.js
@@ -6,6 +6,7 @@ const putUsers = async(req,res)=>{
     const email = req.query?.email
     const userUpdate = await Users.findOneAndUpdate({email },req.body,{new:true});
     res.send(userUpdate);
+
     // console.log(userUpdate) 
 }
 
