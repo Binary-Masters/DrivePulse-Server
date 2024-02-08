fd2f3e1a7f7a830767db095df5def0c12f1eba9b
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2024-02-08 10:05:55 +0600

    clear terminal

--- src/controllers/users/getUsers.controller.js
+++ src/controllers/users/getUsers.controller.js
@@ -4,7 +4,7 @@ const getUsers = async(req,res)=>{
     const email = req.query?.email
     const user = await Users.findOne({email });
     res.send(user);
-    console.log(user)  
+    // console.log(user)  
 }
 
 module.exports = getUsers;
--- src/controllers/users/putUser.controller.js
+++ src/controllers/users/putUser.controller.js
@@ -6,7 +6,7 @@ const putUsers = async(req,res)=>{
     const email = req.query?.email
     const userUpdate = await Users.findOneAndUpdate({email },req.body,{new:true});
     res.send(userUpdate);
-    console.log(userUpdate) 
+    // console.log(userUpdate) 
 }
 
 module.exports = putUsers;
