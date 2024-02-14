7ed96b2043c4ed1ccbabaee3fd7092129620f6de
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-02-15 00:19:02 +0600

    user search data get by search text and user uid

--- src/routes/FilesRoute/filesRouter.js
+++ src/routes/FilesRoute/filesRouter.js
@@ -3,6 +3,7 @@ const getFiles = require("../../controllers/files/getFiles.controller");
 const postFiles = require("../../controllers/files/postFiles.controller");
 const deleteFiles = require("../../controllers/files/deleteFiles.controller");
 const lookup = require("../../controllers/files/lookup.controller");
+const getSearchFiles = require("../../controllers/files/getSearchFiles.controller");
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get("/files", getFiles);
 router.post("/files", postFiles);
 router.post("/files/lookup", lookup)
 router.delete("/files", deleteFiles);
+router.get('/get-search-files',getSearchFiles);
 
 module.exports = router;
