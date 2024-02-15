4c77825209be26c3b077435518805861f8560a47
Morsed <mdmorsedalam324@gmail.com>
2024-02-15 13:09:17 +0600

    Some Modification

--- src/controllers/files/postFiles.controller.js
+++ src/controllers/files/postFiles.controller.js
@@ -1,22 +1,21 @@
 const Filenode = require("../../models/filenode/filenode.model");
 
-const postFiles = async(req, res, next) => {
-	const fileMetadata = req.body;
-	
-	// Spliting path string by directory
-	const pathArr = fileMetadata.fullPath.split("/")
-	fileMetadata.rootDirectory = pathArr[0] + "/";
-	
-	pathArr.pop() // Removing current node
-	if(fileMetadata.type === "folder") pathArr.pop()
-	pathArr.push("")
-	const parentPath = pathArr.join("/");
-	
-	// Assigning parent directory and root directory
-	fileMetadata.parentPath = parentPath;
-	
-	const result = await Filenode.create(fileMetadata)
-	res.send(result)
-}
+const postFiles = async (req, res, next) => {
+  const fileMetadata = req.body;
+  // Spliting path string by directory
+  const pathArr = fileMetadata.fullPath.split("/");
+  fileMetadata.rootDirectory = pathArr[0] + "/";
+
+  pathArr.pop(); // Removing current node
+  if (fileMetadata.type === "folder") pathArr.pop();
+  pathArr.push("");
+  const parentPath = pathArr.join("/");
+
+  // Assigning parent directory and root directory
+  fileMetadata.parentPath = parentPath;
+
+  const result = await Filenode.create(fileMetadata);
+  res.send(result);
+};
 
 module.exports = postFiles;
