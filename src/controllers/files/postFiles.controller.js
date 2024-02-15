564de2f468ba2425b8cc429869242641ee724e35
Morsed <mdmorsedalam324@gmail.com>
2024-02-16 01:01:04 +0600

    Notification System done

--- src/controllers/files/postFiles.controller.js
+++ src/controllers/files/postFiles.controller.js
@@ -1,25 +1,6 @@
 const Filenode = require("../../models/filenode/filenode.model");
 const convertMetadataToNode = require("../../utils/convertMetadataToNode");
 
-<<<<<<< HEAD
-const postFiles = async (req, res, next) => {
-  const fileMetadata = req.body;
-  // Spliting path string by directory
-  const pathArr = fileMetadata.fullPath.split("/");
-  fileMetadata.rootDirectory = pathArr[0] + "/";
-
-  pathArr.pop(); // Removing current node
-  if (fileMetadata.type === "folder") pathArr.pop();
-  pathArr.push("");
-  const parentPath = pathArr.join("/");
-
-  // Assigning parent directory and root directory
-  fileMetadata.parentPath = parentPath;
-
-  const result = await Filenode.create(fileMetadata);
-  res.send(result);
-};
-=======
 const postFiles = async(req, res, next) => {
 	const fileMetadata = req.body;
 	
@@ -28,6 +9,5 @@ const postFiles = async(req, res, next) => {
 	const result = await Filenode.create(node)
 	res.send(result)
 }
->>>>>>> 78e74824dd7e2f92184770bfe9dc683205f93a5f
 
 module.exports = postFiles;
