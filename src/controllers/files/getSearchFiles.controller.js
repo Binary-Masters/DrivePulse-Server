2dc6fa38d417070fb657fa527fe326247be16aa0
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-02-16 14:20:47 +0600

    Search data found and there has no error

--- src/controllers/files/getSearchFiles.controller.js
+++ src/controllers/files/getSearchFiles.controller.js
@@ -9,10 +9,10 @@ const getSearchFiles = async (req, res) => {
   const userId = data.userId;
   const filter = {
     "owner.uid": userId,
-    // name: {$regex: new RegExp(searchText,'i')},
-    name: searchText
+    name: { $regex: new RegExp(searchText, "i") },
   };
   const result = await Filenode.find(filter);
+  console.log(result);
   res.send(result);
 };
 
