5e84b73ae44f457e6fc1789dfed178c0bbbe422c
Morsed <mdmorsedalam324@gmail.com>
2024-02-20 23:56:35 +0600

    patch trush data

---
+++ src/controllers/files/patchStoreFiles.control.js
@@ -0,0 +1,23 @@
+const Filenode = require("../../models/filenode/filenode.model");
+
+const patchStoreFiles = async (req, res, next) => {
+  const id = req.query.id;
+  const data = req.body;
+
+  const filter = {
+    _id: id, // To get files under a specific folder
+  };
+  const update = {
+    $set: {
+      "owner.store": data.store,
+    },
+  };
+  const options = {
+    multi: true, // Assuming you want to update multiple documents if they match the filter
+  };
+
+  const result = await Filenode.updateOne(filter, update, options);
+  res.send(result);
+};
+
+module.exports = patchStoreFiles;
