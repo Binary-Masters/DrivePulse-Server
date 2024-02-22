0d36d297145dae8932019921f3728c15534d0a3a
= <=>
2024-02-22 17:35:44 +0600

    [fix](filenode): Added previously deleted filenode schema

---
+++ src/models/filenode/filenode.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const filenodeSchema = new Schema({
+	checksum: { type: String },
+	owner: {
+		uid: { type: String, required: true },
+		email: { type: String, required: true },
+		status: { type: Number },
+		store: { type: String },
+	},
+	type: { type: String, required: true },
+	bucket: { type: String, required: true },
+	fullPath: {
+		// Points to cloud location ( must be synced with cloud )
+		type: String,
+		required: true,
+	},
+	name: { type: String, required: true },
+	size: { type: Number, required: true },
+	thumbnail: {
+		Type: String,
+		required: true,
+		default: "",
+	},
+	contentType: { type: String, required: true },
+	timeCreated: {
+		type: String,
+		default: new Date().toISOString(),
+		required: true,
+	},
+	updated: {
+		type: String,
+		default: new Date().toISOString(),
+		required: true,
+	},
+	rootDirectory: { type: String, required: true },
+	parentPath: {
+		// To mimic folder management system ( Changable )
+		type: String,
+		required: true,
+	},
+});
+
+const Filenode = model("filenode", filenodeSchema);
+module.exports = Filenode;
