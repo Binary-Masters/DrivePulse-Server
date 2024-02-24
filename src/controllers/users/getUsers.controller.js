1e5df6c4ccc16ad9cf1a0de56576cf202f2909cb
Morsed <mdmorsedalam324@gmail.com>
2024-02-24 15:25:45 +0600

    Socket Server Added In Here

--- src/controllers/users/getUsers.controller.js
+++ src/controllers/users/getUsers.controller.js
@@ -1,12 +1,12 @@
 const Users = require("../../models/users/users.model");
 
-const getUsers = async(req,res)=>{
-    const email = req.query?.email
-    console.log(email);
-    if(!email || email === undefined) return res.status(500).json({message:"email not found"})
-    const user = await Users.findOne({email });
-    res.send(user);
-    // console.log(user)  
-}
+const getUsers = async (req, res) => {
+  const email = req.query?.email;
+  if (!email || email === undefined)
+    return res.status(500).json({ message: "email not found" });
+  const user = await Users.findOne({ email });
+  res.send(user);
+  // console.log(user)
+};
 
 module.exports = getUsers;
