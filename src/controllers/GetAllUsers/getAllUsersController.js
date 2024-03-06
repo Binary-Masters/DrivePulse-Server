2c107c5d10124260ceee839c7b25986c70c44b66
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-03-06 11:00:54 +0600

    Token verification added in file,user and chat route

--- src/controllers/GetAllUsers/getAllUsersController.js
+++ src/controllers/GetAllUsers/getAllUsersController.js
@@ -1,6 +1,7 @@
 const Users = require("../../models/users/users.model");
 
 const getAllUsers = async(req,res)=>{
+    console.log(req.user);
     const result = await Users.find()
     res.send(result);
 }
--- src/routes/MessageRouter/MessageRouter.js
+++ src/routes/MessageRouter/MessageRouter.js
@@ -1,11 +1,12 @@
 const addMessage = require("../../controllers/message/addMessage.controlar.js");
 const getMessages = require("../../controllers/message/getMessage.controller.js");
+const verifyToken = require("../../middlewares/verifyToken.js");
 
 const router = require("express").Router()
 
 
 // messages
 router.post("/message", addMessage)
-router.get("/message/:chatId", getMessages)
+router.get("/message/:chatId",verifyToken, getMessages)
 
 module.exports = router;
